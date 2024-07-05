f472864220a1728936f979528260abaa1c97add8
joenggyun.yu <jeonggyun@gmail.com>
2024-07-05 10:12:03 +0900

    Implemeting Read

--- DeviceDriver/DeviceDriver.cpp
+++ DeviceDriver/DeviceDriver.cpp
@@ -1,4 +1,22 @@
+#include <stdexcept>
+
 #include "DeviceDriver.h"
+
+using namespace std;
+
+class ReadFailException : public std::exception {
+public:
+    ReadFailException(const char* message) : msg(message) {}
+
+    // Override the what() method to provide exception message
+    virtual const char* what() const noexcept {
+        return msg;
+    }
+
+private:
+    const char* msg;
+};
+
 
 DeviceDriver::DeviceDriver(FlashMemoryDevice* hardware) : m_hardware(hardware)
 {}
@@ -6,7 +24,18 @@ DeviceDriver::DeviceDriver(FlashMemoryDevice* hardware) : m_hardware(hardware)
 int DeviceDriver::read(long address)
 {
     // TODO: implement this method properly
-    return (int)(m_hardware->read(address));
+
+    bool isSame = true;
+    int preResult = (int)(m_hardware->read(address));
+    for (int i = 0; i < 4; i++) {
+        int nowResult = (int)(m_hardware->read(address));
+        if (preResult != nowResult) isSame = false;
+        preResult = nowResult;
+    }
+
+    if (isSame == false) throw ReadFailException("Different read value !!");
+
+    return preResult;
 }
 
 void DeviceDriver::write(long address, int data)
--- Sample-Test1/test.cpp
+++ Sample-Test1/test.cpp
@@ -1,7 +1,39 @@
 #include "gtest/gtest.h"
 #include "gmock/gmock.h"
+#include "../DeviceDriver/FlashMemoryDevice.h"
+#include "../DeviceDriver/DeviceDriver.cpp"
 
-TEST(TestCaseName, TestName) {
-  EXPECT_EQ(1, 1);
-  EXPECT_TRUE(true);
+using namespace std;
+using namespace testing;
+
+// 목클래스
+class FlashMock : public FlashMemoryDevice {
+public:
+	MOCK_METHOD(unsigned char, read, (long address), (override));
+	MOCK_METHOD(void, write, (long address, unsigned char data), (override));
+};
+
+class DriverFixture : public Test {
+public:
+	FlashMock mk;
+};
+
+TEST_F(DriverFixture, TestFiveTimesRead) {
+	EXPECT_CALL(mk, read(0xA)).Times(5);
+
+	DeviceDriver* dd = new DeviceDriver(&mk);
+	dd->read(0xA);
+}
+
+TEST_F(DriverFixture, TestFiveTimesReadDiffValue) {
+	EXPECT_CALL(mk, read(0xA))
+		.Times(5)
+		.WillOnce(Return(1))
+		.WillOnce(Return(1))
+		.WillOnce(Return(2))
+		.WillRepeatedly(Return(1));
+
+	DeviceDriver* dd = new DeviceDriver(&mk);
+
+	EXPECT_THROW(dd->read(0xA), ReadFailException);
 }
